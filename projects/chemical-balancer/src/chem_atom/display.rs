1f3270899bf66c3e28441bfd899dbc015f1fcafb
Aster <aster@vers.site>
2022-08-01 19:18:07 +0000

    🎀 Render equations with multinomial coefficients as cases

--- projects/chemical-balancer/src/chem_atom/display.rs
+++ projects/chemical-balancer/src/chem_atom/display.rs
@@ -1,6 +1,6 @@
-use crate::ChemicalBalancer;
+use crate::{ChemicalBalancer, ChemicalTerm};
 use latexify::Latexify;
-use mathml_core::{MathML, MathNumber, MathOperator, MathRow};
+use mathml_core::{helpers::cases, MathML, MathNumber, MathOperator, MathRow};
 use std::fmt::Write;
 
 impl Latexify for ChemicalBalancer {
@@ -18,41 +18,44 @@ impl Latexify for ChemicalBalancer {
 impl ChemicalBalancer {
     pub fn render_mathml(&self, solved: &[Vec<f64>]) -> MathML {
         if solved.len() > 1 {
-            todo!()
+            let rows: Vec<_> = solved.iter().map(|coefficients| self.render_mathml_row(coefficients)).collect();
+            cases(rows)
         }
         else {
             let coefficients = solved[0].as_slice();
-            let mut lhs = Vec::new();
-            let mut rhs = Vec::new();
-            for (index, coefficient) in coefficients.iter().enumerate() {
-                let item = self.get_term(index).expect("item index out of range");
-                if coefficient.is_sign_positive() {
-                    rhs.push((coefficient, item));
-                }
-                else if coefficient.is_sign_negative() {
-                    lhs.push((-coefficient, item));
-                }
-                else {
-                    // zero, drop
-                }
-            }
-            let mut eqation: Vec<MathML> = vec![];
-            for (index, (coefficient, item)) in lhs.into_iter().enumerate() {
-                if index != 0 {
-                    eqation.push(MathOperator::new("+").into())
-                }
-                eqation.push(MathNumber::new(coefficient).into());
-                eqation.push(item.into())
-            }
-            eqation.push(MathOperator::new("=").into());
-            for (index, (coefficient, item)) in rhs.into_iter().enumerate() {
-                if index != 0 {
-                    eqation.push(MathOperator::new("+").into())
-                }
-                eqation.push(MathNumber::new(coefficient).into());
-                eqation.push(item.into())
-            }
-            MathRow::new(eqation).into()
+            self.render_mathml_row(coefficients)
         }
+    }
+
+    fn render_mathml_row(&self, coefficients: &[f64]) -> MathML {
+        let mut lhs = Vec::new();
+        let mut rhs = Vec::new();
+        for (index, coefficient) in coefficients.iter().enumerate() {
+            let item = self.get_term(index).expect("item index out of range");
+            if coefficient.is_sign_positive() {
+                rhs.push((*coefficient, item));
+            }
+            else if coefficient.is_sign_negative() {
+                lhs.push((-*coefficient, item));
+            }
+            else {
+                // zero, drop
+            }
+        }
+        let mut eqation: Vec<MathML> = vec![];
+        push_pair(&mut eqation, lhs);
+        eqation.push(MathOperator::new("=").into());
+        push_pair(&mut eqation, rhs);
+        MathRow::new(eqation).into()
+    }
+}
+
+fn push_pair(eqation: &mut Vec<MathML>, terms: Vec<(f64, ChemicalTerm)>) {
+    for (index, (coefficient, item)) in terms.into_iter().enumerate() {
+        if index != 0 {
+            eqation.push(MathOperator::new("+").into())
+        }
+        eqation.push(MathNumber::new(coefficient).into());
+        eqation.push(item.into())
     }
 }
--- projects/chemical-balancer/tests/mathml/mod.rs
+++ projects/chemical-balancer/tests/mathml/mod.rs
@@ -22,6 +22,15 @@ pub fn test_benzoic_acid() {
     assert_no_ws(&display_mathml(mathml), include_str!("equation1.xml"))
 }
 
+#[test]
+pub fn test_benzoic_acid2() {
+    let input = ChemicalBalancer::from_str("C + CO + CO2 = O2").unwrap();
+    println!("{}", input.solve_by_mathematica());
+    let solved = cast_isize_to_f64(input.solve_integers());
+    let mathml = input.render_mathml(&solved);
+    assert_no_ws(&display_mathml(mathml), include_str!("equation2.xml"))
+}
+
 fn cast_isize_to_f64(v: Vec<Vec<isize>>) -> Vec<Vec<f64>> {
     v.iter().map(|v| v.iter().map(|&x| x as f64).collect()).collect()
 }
