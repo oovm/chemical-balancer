2a549a7bc6e08d08c015da03084af020285b9dd7
aster <192608617@qq.com>
2022-08-10 19:55:29 +0000

    🐣 Add typescript version

--- projects/chemical-ui/website/main.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_snake_case)]
-// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
-use chemical_ui::Editor;
-
-fn main() {
-    // launch the web app
-    dioxus_web::launch(Editor);
-}
