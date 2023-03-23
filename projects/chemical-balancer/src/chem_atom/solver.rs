69e2321fdb43d13b46a7319e2cfa800377ad8bbb
Aster <aster@vers.site>
2023-03-23 21:03:08 +0800

    Fix solving null space

--- projects/chemical-balancer/src/chem_atom/solver.rs
+++ projects/chemical-balancer/src/chem_atom/solver.rs
@@ -1,75 +1,55 @@
 use super::*;
 
-fn null_space(input: &[Vec<f64>]) -> Result<Vec<Vec<f64>>, String> {
-    let n = input[0].len();
-    let m = input.len();
-
-    let mut rref = input.to_vec();
-    let mut lead = 0;
-
-    for row in 0..m {
-        if n <= lead {
-            break;
-        }
-        let mut i = row;
-        while rref[i][lead] == 0.0 {
-            i += 1;
-            if m == i {
-                i = row;
-                lead += 1;
-                if n == lead {
-                    break;
-                }
-            }
-        }
-        if n == lead {
-            break;
-        }
-
-        rref.swap(row, i);
-
-        let lv = rref[row][lead];
-        rref[row] = rref[row].iter().map(|&x| x / lv).collect();
-
-        for i in 0..m {
-            if i != row {
-                let lv = rref[i][lead];
-                rref[i] = rref[i]
-                    .iter()
-                    .zip(rref[row].iter())
-                    .map(|(&x, &y)| x - y * lv)
-                    .collect();
-            }
-        }
-        lead += 1;
-    }
-
-    let mut null_space_basis = Vec::new();
-    for i in 0..n {
-        let mut is_zero_column = true;
-        let mut basis_vector = vec![0.0; n];
-        for j in 0..m {
-            if rref[j][i] != 0.0 {
-                is_zero_column = false;
-                basis_vector[j] = -rref[j][i];
+fn null_space(mut matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let n = matrix.len();
+    let m = matrix[0].len();
+    let mut null_basis = vec![];
+    let mut pivot_cols = vec![];
+    let mut row = 0;
+    for col in 0..m {
+        let mut pivot_row = None;
+        for i in row..n {
+            if matrix[i][col] != 0.0 {
+                pivot_row = Some(i);
                 break;
             }
         }
-        if is_zero_column {
-            basis_vector[i] = 1.0;
+        if let Some(pivot_row) = pivot_row {
+            if pivot_row != row {
+                matrix.swap(pivot_row, row);
+            }
+            pivot_cols.push(col);
+            for i in (row + 1)..n {
+                let factor = matrix[i][col] / matrix[row][col];
+                for j in col..m {
+                    matrix[i][j] -= factor * matrix[row][j];
+                }
+            }
+            row += 1;
         }
-        null_space_basis.push(basis_vector);
     }
-
-    Ok(null_space_basis)
+    for col in 0..m {
+        if !pivot_cols.contains(&col) {
+            let mut null_vec = vec![0.0; m];
+            null_vec[col] = 1.0;
+            for (i, &pivot_col) in pivot_cols.iter().enumerate() {
+                let factor = matrix[i][col];
+                null_vec[pivot_col] = -factor;
+            }
+            null_basis.push(null_vec);
+        }
+    }
+    null_basis
 }
+
 
 #[test]
 fn test() {
-    let out = null_space(&[
+    let out = null_space(vec![
         vec![1.0, 0.0, 0.0, -1.0],
-        vec![0.0, 2.0, -1.0,0.0],
-        vec![-2.0,0.0,1.0,0.0],
+        vec![0.0, 2.0, -1.0, 0.0],
+        vec![-2.0, 0.0, 1.0, 0.0],
     ]);
-    println!("{:#?}", out)
+    println!("{:#?}", out);
+    assert_eq!(out, vec![vec![1.0, 1.0, 2.0, 1.0]])
 }
