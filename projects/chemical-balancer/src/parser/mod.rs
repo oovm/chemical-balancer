3dd5518c952d61fc703381b58cbcfcb95c77a089
Aster <aster@vers.site>
2023-03-23 20:27:30 +0800

    Support parse parentheses

--- projects/chemical-balancer/src/parser/mod.rs
+++ projects/chemical-balancer/src/parser/mod.rs
@@ -1,5 +1,5 @@
 use std::str::FromStr;
-use pex::{ParseResult, ParseState};
+use pex::{ParseResult, ParseState, StopBecause};
 use crate::Compound;
 
 impl FromStr for Compound {
@@ -13,25 +13,42 @@ impl FromStr for Compound {
 
 impl Compound {
     pub fn parse(state: ParseState) -> ParseResult<Self> {
-        todo!()
-    }
-
-    pub fn parse_parentheses(state: ParseState) -> ParseResult<Compound> {
-        let (state, _) = state.match_char('(')?;
-        let (state, compound) = Self::parse(state)?;
-        let (state, _) = state.match_char(')')?;
+        let (state, compound) = state.match_repeats(|s|
+            {
+                let (state, _) = s.match_optional(parse_whitespace)?;
+                state.begin_choice()
+                    .maybe(Self::parse_parentheses_count)
+                    .maybe(Self::parse_atom_count)
+                    .end_choice()
+            }
+        )?;
+        state.skip(parse_whitespace);
+        let (state, number) = state.match_optional(parse_decimal)?;
         state.finish(Compound::Compound {
             group: Default::default(),
-            compound: vec![compound],
-            count: 1.0,
+            compound,
+            count: number.unwrap_or(1.0),
         })
+    }
+
+
+    fn parse_parentheses_count(state: ParseState) -> ParseResult<Compound> {
+        let (state, _) = state.match_char('(')?;
+        state.skip(parse_whitespace);
+        let (state, cs) = state.match_repeats(Self::parse)?;
+        state.skip(parse_whitespace);
+        let (state, _) = state.match_char(')')?;
+        state.skip(parse_whitespace);
+        let (state, number) = state.match_optional(parse_decimal)?;
+        state.finish(Compound::parentheses(cs, number.unwrap_or(1.0)))
     }
 
 
     fn parse_atom_count(state: ParseState) -> ParseResult<Compound> {
         let (state, atom) = Self::parse_atom(state)?;
-        let (state, _) = state.match_optional(parse_whitespace)?;
+        state.skip(parse_whitespace);
         let (state, number) = state.match_optional(parse_decimal)?;
+        println!("{} {:?}", atom, number);
         state.finish(Compound::Atom {
             atom,
             count: number.unwrap_or(1.0),
@@ -52,7 +69,14 @@ impl Compound {
 }
 
 fn parse_whitespace(state: ParseState) -> ParseResult<()> {
-    state.rest_text.find(|c: char| !c.is_ascii_whitespace()).map_or(state.finish(()), |i| state.advance(i).finish(()))
+    match state.rest_text.find(|c: char| !c.is_ascii_whitespace()) {
+        None => {
+            state.finish(())
+        }
+        Some(s) => {
+            state.advance(s).finish(())
+        }
+    }
 }
 
 // decimal = integer (. integer)?
@@ -65,8 +89,7 @@ fn parse_decimal(state: ParseState) -> ParseResult<f64> {
     match rest {
         // usize
         Some(s) => {
-            let digits = (s as f64).log10().floor() as usize;
-            state.finish(integer as f64 + s as f64 / 10.0f64.powi(digits as i32))
+            state.finish(build_f64(integer, s))
         }
         None => {
             state.finish(integer as f64)
@@ -74,28 +97,49 @@ fn parse_decimal(state: ParseState) -> ParseResult<f64> {
     }
 }
 
+fn build_f64(integer: usize, decimal: usize) -> f64 {
+    let mut out = integer as f64;
+    let mut decimal = decimal as f64;
+    while decimal > 0.0 {
+        decimal /= 10.0;
+        out += decimal;
+    }
+    out
+}
+
 
 // integer = (_? + digit)+
 fn parse_integer(state: ParseState) -> ParseResult<usize> {
-    let (state, integer) = state.match_repeats(parse_digit)?;
-    let mut out = 0;
-    for c in integer {
-        out = out * 10 + (c as usize - '0' as usize);
+    let mut offset = 0;
+    let mut has_number = false;
+    for c in state.rest_text.chars() {
+        match c {
+            '_' => {
+                offset += 1;
+            }
+            '0'..='9' => {
+                has_number = true;
+                offset += 1;
+            }
+            _ => {
+                break;
+            }
+        }
     }
-    state.finish(out)
+    if offset.eq(&0) || !has_number {
+        Err(StopBecause::MissingString { message: "Except digits", position: state.start_offset })?
+    } else {
+        let integer = state.rest_text[..offset].parse().unwrap();
+        state.advance(offset).finish(integer)
+    }
+
 }
 
-// digit = _? + decimal_number
-fn parse_digit(state: ParseState) -> ParseResult<char> {
-    let (state, _) = state.match_optional(|s| s.match_char('_'))?;
-    let (state, digit) = state.match_char_if(|c| c.is_ascii_digit(), "Digit")?;
-    state.finish(digit)
-}
 
 
 #[test]
 fn test() {
-    let co2 = ParseState::new("CO _2");
-    let co2 = Compound::parse_atom_count(co2);
-    println!("{:?}", co2)
+    let co2 = ParseState::new("S2(CO3)");
+    let co2 = Compound::parse(co2);
+    println!("{:#?}", co2)
 }
